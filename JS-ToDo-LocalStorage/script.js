f798f762285829606269131d5b50587e961b2150
Akshata Jog <akshatacontact@gmail.com>
2025-03-03 14:18:49 +0400

    Step 51 completed

--- JS-ToDo-LocalStorage/script.js
+++ JS-ToDo-LocalStorage/script.js
@@ -24,6 +24,8 @@ const addOrUpdateTask = () => {
 
   if (dataArrIndex === -1) {
     taskData.unshift(taskObj);
+  } else {
+    taskData[dataArrIndex] = taskObj;
   }
   updateTaskContainer();
   reset();
@@ -38,11 +40,33 @@ const updateTaskContainer = () => {
           <p><strong>Title:</strong> ${title}</p>
           <p><strong>Date:</strong> ${date}</p>
           <p><strong>Description:</strong> ${description}</p>
-          <button type="button" class="btn">Edit</button>
-          <button type="button" class="btn">Delete</button> 
+          <button onclick="editTask(this)" type="button" class="btn">Edit</button>
+          <button onclick="deleteTask(this)" type="button" class="btn">Delete</button> 
         </div>
       `;
   });
+};
+
+const deleteTask = (buttonEl) => {
+  const dataArrIndex = taskData.findIndex(
+    (item) => item.id === buttonEl.parentElement.id
+  );
+
+  buttonEl.parentElement.remove();
+  taskData.splice(dataArrIndex, 1);
+};
+
+const editTask = (buttonEl) => {
+  const dataArrIndex = taskData.findIndex((item) => {
+    item.id === buttonEl.parentElement.id;
+  });
+  currentTask = taskData[dataArrIndex];
+  titleInput.value = currentTask.title;
+  dateInput.value = currentTask.date;
+  descriptionInput.value = currentTask.description;
+
+  addOrUpdateTaskBtn.innerText = "Update Task";
+  taskForm.classList.toggle("hidden");
 };
 
 const reset = () => {
