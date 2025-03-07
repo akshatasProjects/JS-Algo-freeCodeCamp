4c41138705ce78dde2714968093f4e66b3ecc9dc
Akshata Jog <akshatacontact@gmail.com>
2025-03-07 13:36:46 +0400

    done 35 steps

--- JS-DecimalToBinaryConvertor/script.js
+++ JS-DecimalToBinaryConvertor/script.js
@@ -1,15 +1,3 @@
 const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
-
-const checkUserInput = () => {
-  if (!numberInput.value || parseInt(numberInput.value)) {
-  }
-  convertBtn.addEventListener("click", checkUserInput);
-};
-
-numberInput.addEventListener("keydown", (e) => {
-  if (e.key === "Enter") {
-    checkUserInput();
-  }
-});
