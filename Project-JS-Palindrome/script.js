eb0024d62b47fbe25c5e197e324007b056e574eb
Akshata Jog <akshatacontact@gmail.com>
2025-03-19 13:40:35 +0400

    done with palindrome

--- Project-JS-Palindrome/script.js
+++ Project-JS-Palindrome/script.js
@@ -1,20 +1,21 @@
-function palindrome() {
-  const inputText = document.getElementById("text-input").value;
-  if (!inputText) {
-    alert("Please input a value");
-  }
-  const result = document.getElementById("result");
-  const inputArr = inputText
-    .toLowerCase()
-    .split("")
-    .filter((char) => !char.match(/[\(\)\\.\s_,-]/gi));
+const textInput = document.getElementById("text-input");
+const chkButton = document.getElementById("check-btn");
+const result = document.getElementById("result");
 
-  for (let i = 0; i < inputArr.length; i++) {
-    if (inputArr[i] !== inputArr[inputArr.lenth - i - 1]) {
-      console.log(inputArr[i]);
-      result.innerText = `${inputText} is not a palindrome`;
-      return;
-    }
+chkButton.addEventListener("click", () => {
+  let lowerReplaced = textInput.value.toLowerCase().replace(/[^a-z0-9]/g, "");
+
+  if (textInput.value === "") {
+    alert("Please input a value");
+  } else if (textInput.value.length === 1) {
+    result.innerText = `${textInput.value} is a Palindrome`;
+  } else if (lowerReplaced === [...lowerReplaced].reverse().join("")) {
+    result.innerText = `${textInput.value} is a palindrome`;
+  } else {
+    result.innerText = `${textInput.value} is not a palindrome`;
   }
-  result.innerText = `${inputText} is  a palindrome`;
-}
+});
+
+// Spread can be replaced with split also
+// For simple strings, both [...] and .split('') are fine.
+// For Unicode support(emoji), use Array.from().
